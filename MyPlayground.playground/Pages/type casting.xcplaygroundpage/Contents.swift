b6d69360a4e433bdd086c83e138936d29ebc3dd8
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-04-02 11:36:08 +0900

    type casting

---
+++ MyPlayground.playground/Pages/type casting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+/* 타입 캐스팅?
+ 인스턴스의 타입을 확인하거나 어떠한 클래스의 인스턴스를 해당 클래스 계층 구조의 슈퍼 클래스나 서브 클래스로 취급하는 방법
+ */
+
+// is, as 연산자로 구현 - 값이 타입 확인, 다른 타입으로 변환하는 데 사용
+
+import Foundation
+
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "기생충", director: "봉준호"),
+    Song(name: "Butter", artist: "BTS"),
+    Movie(name: "올드보이", director: "박찬욱"),
+    Song(name: "Wonderwall", artist: "Oasis"),
+    Song(name: "Rain", artist: "이적")
+]
+
+var movieCount = 0
+var songCount = 0
+
+for item in library {
+    if item is Movie {
+        movieCount += 1
+    } else if item is Song {
+        songCount += 1
+    }
+}
+
+print("Media library contains \(movieCount) movies and \(songCount) songs")
+
+
+/* down casting
+ 서브 클래스를 참조하도록 형 변환
+ - as? optional 반환 (성공확신이 들 때만 사용해야 함.  런타임 에러로 코드 종료)
+ - as! 강제로 unwrapping 하여 반환
+*/
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: \(movie.name), dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: \(song.name), by \(song.artist)")
+    }
+}
