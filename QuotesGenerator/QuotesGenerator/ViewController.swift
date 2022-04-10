bb9dabd4880bb38b28cf46662facd373fe9ac70e
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-04-10 20:45:19 +0900

    practice 01 - 명언 제조기 UI 구성

---
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by Sootully on 2022/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
