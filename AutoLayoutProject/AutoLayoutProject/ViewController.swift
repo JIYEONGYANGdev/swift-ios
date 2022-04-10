b3a2b1003176fc05874a03c8e2e998f6acdc6b87
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-04-10 17:12:03 +0900

    connect viewcontroller swift code with ui view

--- AutoLayoutProject/AutoLayoutProject/ViewController.swift
+++ AutoLayoutProject/AutoLayoutProject/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func tabChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("color change button clicked")
+    }
+    
 }
 
